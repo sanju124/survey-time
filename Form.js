faf19944e987ee8890816cdaa90828be31460d32
sanju124 <84239360+sanju124@users.noreply.github.com>
2021-05-16 05:41:33 +0530

    Style.css
    Ok now

---
+++ Form.js
@@ -0,0 +1,22 @@
+var form = document.getElementById("sanju124");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("sanju125");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
+  
